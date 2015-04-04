0119b84bd3806253f48e049d2ad94a34763555e5
ashiver <anthony.lee.shiver@gmail.com>
2015-04-04 20:19:39 +0000

    Commit 3

--- snippets.py
+++ snippets.py
@@ -13,11 +13,34 @@ def put(name, snippet):
     return name, snippet
 
 def get(name):
-    """Retrieve the snippet with a given name.
+    """
+    Retrieve the snippet with a given name.
     
-    If there is no such snipper...
+    If there is no such snippet, report that snippet does not exist.
     
     Returns the snippet.
     """
     logging.error("FIXME: Unimplemented - get({!r})".format(name))
-    return "Sorry, {!r} does not exist.".format(name)
+    return ""
+
+def rem(name):
+    """
+    Delete the snippet with the given name.
+    
+    If snippet exists, report snippet and asks for confirmation. If confirmation, delete snippet.
+    
+    If there is no such snippet, report that snippet does not exist.
+    """
+    logging.error("FIXME: Unimplemented - rem({!r})".format(name))
+    return ""
+
+def edt(name, snippet):
+    """
+    Edits snippet with given name.
+    
+    If snippet exists, report old snippet and new snippet, ask for confirmation. If confirmation, replace old with new.
+    
+    If there is no such snippet, report that snippet does not exist.
+    """
+    logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
+    return name, snippet
