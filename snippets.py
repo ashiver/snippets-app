5cd29654598a9a4299df7dbf6f6fa67b1eb8bbc3
ashiver <anthony.lee.shiver@gmail.com>
2015-04-04 20:49:21 +0000

    Commit 5

--- snippets.py
+++ snippets.py
@@ -1,4 +1,4 @@
-import logging
+import logging, argparse, sys
 
 # Set the log output file, and the log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
@@ -42,5 +42,14 @@ def edt(name, snippet):
     
     If there is no such snippet, report that snippet does not exist.
     """
-    logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
+    logging.error("FIXME: Unimplemented - edt({!r}, {!r})".format(name, snippet))
     return name, snippet
+
+def main():
+    """Main function"""
+    logging.info("Constructing parser")
+    parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+    arguments = parser.parse_args(sys.argv[1:])
+    
+if __name__ == "__main__":
+    main()
